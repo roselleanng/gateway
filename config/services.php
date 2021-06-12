dc73e2c3b49fcd9b569617220c161048dbcd2488
unknown <roselleannguzman14@gmail.com>
2021-06-12 09:35:42 +0800

    Update as of June 12 2021

--- config/services.php
+++ config/services.php
@@ -3,10 +3,12 @@
 return [
     'books' => [
         'base_uri' => env('BOOKS_SERVICE_BASE_URL'),
+        'secret' => env('BOOKS_SERVICE_SECRET'),
     ],
 
     'authors' => [
         'base_uri' => env('AUTHORS_SERVICE_BASE_URL'),
+        'secret' => env('AUTHORS_SERVICE_SECRET'),
     ],
 
 ];
